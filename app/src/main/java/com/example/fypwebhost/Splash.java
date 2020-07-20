e16eae2f865a520f1b55b5460b6841de4a8607ff
Muhammad hamza <m.hamza7459@gmail.com>
2020-07-20 10:33:43 +0500

    removing member. deleting class in progress.

--- app/src/main/java/com/example/fypwebhost/Splash.java
+++ app/src/main/java/com/example/fypwebhost/Splash.java
@@ -1,4 +1,4 @@
-package com.example.textsimilaritydetector;
+package com.example.fypwebhost;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
--- app/src/main/java/com/example/fypwebhost/Student_Activity.java
+++ app/src/main/java/com/example/fypwebhost/Student_Activity.java
@@ -1,4 +1,4 @@
-package com.example.textsimilaritydetector;
+package com.example.fypwebhost;
 
 import android.content.Context;
 import android.content.Intent;
